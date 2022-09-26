7c3d2673ebbe1ea76930b15da191c05996e8a0e7
Jorge Rivera <jrivera@VESSELARG.local>
2022-09-26 14:20:49 -0300

    working on deploying menues

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("app Working")
+console.log("app Working ???");
+
+// function hideMenu(id) {
+//     console.log("arrow clicked!")
+//     console.log(id)
+
+//     menu = document.getElementById(id);
+//     menu.classList.toogle("hideMenu")
+// }
+
+function showMenu(id) {
+    let menu = document.getElementById(id);
+    menu.classList.add("hideMenu");
+}
+
+function hideMenu(id) {
+    let menu = document.getElementById(id);
+    menu.classList.remove("hideMenu");
+}
+
+
+
